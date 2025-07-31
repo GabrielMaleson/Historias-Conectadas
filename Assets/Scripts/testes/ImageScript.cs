2930ca94ca058cac1077d8264c9b622b3c0393f2
GabrielMaleson <167476506+GabrielMaleson@users.noreply.github.com>
2025-07-31 16:26:51 -0300

    script check at home

--- Assets/Scripts/testes/ImageScript.cs
+++ Assets/Scripts/testes/ImageScript.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
-public class ImageScript : MonoBehaviour
+[System.Serializable]
+public class TaggedImage
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Image image;
+    public string imageTag;
+}
 
-    // Update is called once per frame
-    void Update()
+public class ImageTagManager : MonoBehaviour
+{
+    public List<TaggedImage> taggedImages = new List<TaggedImage>();
+
+    // Optional: Add a method to find an image by its tag
+    public Image GetImageByTag(string tag)
     {
-        
+        foreach (var taggedImage in taggedImages)
+        {
+            if (taggedImage.imageTag == tag)
+            {
+                return taggedImage.image;
+            }
+        }
+        return null;
     }
 }
