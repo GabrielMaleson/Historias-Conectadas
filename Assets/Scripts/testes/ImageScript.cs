6ec72ace0760e31a5f787a9d3203cd01ee7ec602
GabrielMaleson <167476506+GabrielMaleson@users.noreply.github.com>
2025-08-01 01:37:18 -0300

    WE ARE DOING IT!!!

--- Assets/Scripts/testes/ImageScript.cs
+++ Assets/Scripts/testes/ImageScript.cs
@@ -1,28 +1,97 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using Yarn.Unity;
 
-[System.Serializable]
-public class TaggedImage
+public class StaticImageTagManager : MonoBehaviour
 {
-    public Image image;
-    public string imageTag;
-}
+    [Tooltip("List of UI Images")]
+    public List<Image> images = new List<Image>();
 
-public class ImageTagManager : MonoBehaviour
-{
-    public List<TaggedImage> taggedImages = new List<TaggedImage>();
+    [Tooltip("List of tags corresponding to each image")]
+    public List<string> imageTags = new List<string>();
 
-    // Optional: Add a method to find an image by its tag
-    public Image GetImageByTag(string tag)
+    private static StaticImageTagManager instance;
+    private static Dictionary<string, Image> activeSprites = new Dictionary<string, Image>();
+
+    private void Awake()
     {
-        foreach (var taggedImage in taggedImages)
+        if (instance == null)
         {
-            if (taggedImage.imageTag == tag)
+            instance = this;
+        }
+        else
+        {
+            Debug.LogWarning("Multiple StaticImageTagManager instances found. Only one should exist.");
+        }
+    }
+
+    private void OnValidate()
+    {
+        while (images.Count > imageTags.Count)
+        {
+            imageTags.Add("");
+        }
+
+        while (imageTags.Count > images.Count)
+        {
+            imageTags.RemoveAt(imageTags.Count - 1);
+        }
+    }
+
+    [YarnCommand("sprite")]
+    public static void PlaceSprite(string spriteTag, string positionName)
+    {
+        if (instance == null)
+        {
+            Debug.LogError("No StaticImageTagManager instance found in scene!");
+            return;
+        }
+
+        // First remove any existing sprite at this position
+        RemoveSprite(positionName);
+
+        // Find the image by tag
+        Image foundImage = null;
+        for (int i = 0; i < instance.imageTags.Count; i++)
+        {
+            if (instance.imageTags[i] == spriteTag)
             {
-                return taggedImage.image;
+                foundImage = instance.images[i];
+                break;
             }
         }
-        return null;
+
+        if (foundImage == null)
+        {
+            Debug.LogError($"No image found with tag: {spriteTag}");
+            return;
+        }
+
+        // Find the position GameObject
+        GameObject positionObj = GameObject.Find(positionName);
+        if (positionObj == null)
+        {
+            Debug.LogError($"No GameObject found with name: {positionName}");
+            return;
+        }
+
+        // Create a new instance of the image at the target position
+        Image newImage = Instantiate(foundImage, positionObj.transform);
+        newImage.transform.localPosition = Vector3.zero;
+        newImage.gameObject.SetActive(true);
+
+        // Track this active sprite
+        activeSprites[positionName] = newImage;
+    }
+
+    [YarnCommand("removeSprite")]
+    public static void RemoveSprite(string positionName)
+    {
+        if (activeSprites.TryGetValue(positionName, out Image existingImage))
+        {
+            Destroy(existingImage.gameObject);
+            activeSprites.Remove(positionName);
+        }
     }
 }
